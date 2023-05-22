28230db28cf7c466f4ffc09233dd1ac6db21f05d
Samarth <samarthdev138@gmail.com>
2023-05-22 02:15:55 -0700

    Updating file names

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,14 @@
 -- importing the colorscheme, options, and keymaps files
-require("samarth.plugins-setup")
-require("samarth.core.options")
-require("samarth.core.keymaps")
-require("samarth.core.colorscheme")
-require("samarth.plugins.lualine")
-require("samarth.plugins.nvim-tree")
-require("samarth.plugins.nvim-cmp")
-require("samarth.plugins.lsp.mason")
-require("samarth.plugins.lsp.lspconfig")
-require("samarth.plugins.treesitter")
-require("samarth.plugins.telescope")
-require("samarth.plugins.alpha") -- initializing the alpha file
-require("samarth.plugins.autopairs") -- for autopairs 
+require("playground.plugins-setup")
+require("playground.core.options")
+require("playground.core.keymaps")
+require("playground.core.colorscheme")
+require("playground.plugins.lualine")
+require("playground.plugins.nvim-tree")
+require("playground.plugins.nvim-cmp")
+require("playground.plugins.lsp.mason")
+require("playground.plugins.lsp.lspconfig")
+require("playground.plugins.treesitter")
+require("playground.plugins.telescope")
+require("playground.plugins.alpha") -- initializing the alpha file
+require("playground.plugins.autopairs") -- for autopairs 
