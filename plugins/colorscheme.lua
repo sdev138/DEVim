82d7fbfcedd3e8ff2281217d043178b54b258361
sdev138 <samarthdev138@gmail.com>
2024-07-16 03:52:25 -0700

    Major changes with packages and aesthetic customizations

---
+++ plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+-- This is for the catppuccin mocha neovim theme
+return {
+	"catppuccin/nvim",
+	priority = 1000,
+	config = function()
+		-- loading colorscheme
+		vim.cmd([[colorscheme catppuccin-macchiato]])
+	end,
+}
